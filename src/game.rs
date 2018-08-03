f29996f3c125ff5debab5d66daef8b846b1cd58f
mathias234 <mathais234@gmail.com>
2018-08-03 17:45:11 +0200

    Moved resources into /res

--- src/game.rs
+++ src/game.rs
@@ -12,16 +12,16 @@ pub fn start(display: &glium::Display, context: &mut RenderContext) {
     context.clear_b = 0.7;
     context.clear_g = 0.3;
 
-    let monkey_head_model = Model::load_model(&display, "test.obj".to_string());
-    let plane = Model::load_model(&display, "plane.obj".to_string());
+    let monkey_head_model = Model::load_model(&display, "res/test.obj".to_string());
+    let plane = Model::load_model(&display, "res/plane.obj".to_string());
 
     let plane = GameObject::new(
         "plane".to_string(),
         [0.0, -2.0, 0.0],
         [0.0, 0.0, 0.0],
         plane,
-        shader::read_shader_file(&display, "basic"),
-        texture::load_texture(&display, "test.png".to_string()),
+        shader::read_shader_file(&display, "res/basic"),
+        texture::load_texture(&display, "res/test.png".to_string()),
     );
 
     let monkey_head = GameObject::new(
@@ -29,8 +29,8 @@ pub fn start(display: &glium::Display, context: &mut RenderContext) {
         [0.0, 0.0, -10.0],
         [0.0, 0.0, 0.0],
         monkey_head_model,
-        shader::read_shader_file(&display, "basic"),
-        texture::load_texture(&display, "test.png".to_string()),
+        shader::read_shader_file(&display, "res/basic"),
+        texture::load_texture(&display, "res/test.png".to_string()),
     );
 
     context.models.push(monkey_head);
